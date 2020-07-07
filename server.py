ffcb2dfb0116b42e5d6aff2dc3b0eda584448ffa
Samuel Earnest <earnestcsamuel@gmail.com>
2020-07-07 09:25:03 -0600

    sam

--- server.py
+++ server.py
@@ -5,12 +5,17 @@ import time
 import requests
 import json
 
+#Disable insecure request warnings from verify = False in requests
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 class Server:
     def __init__(self):
         self.active = False
         self.gateway_path = './clientportal.gw'
         self.port_host = 'https://localhost:5000/v1/'
         self.process = None
+        self.auth = False
 
     #Starts server
     def start_session(self):
@@ -27,17 +32,33 @@ class Server:
 
     #Checks server state given it is access
     def check_status(self):
+        #Confirm server is active
         resp = requests.post(self.port_host + "portal/tickle", verify = False)
         if(resp.status_code != 200):
-            print('error')
+            print('tickle error')
+        if(resp.json()['iserver']['authStatus']['connected']):
+            print('connected')
+            self.active = True
+        else:
+            print('not connected')
+            self.active = False
 
-        resp = requests.get(self.port_host + "sso/validate", verify = False)
+        #Check validation
+        resp = requests.get(self.port_host + "portal/sso/validate", verify = False)
         if(resp.status_code != 200):
-            print('error')
+            print('validate error')
 
-        resp = requests.post(self.port_host + "iserver/auth/server", verify = False)
+        #Check authentication status
+        resp = requests.post(self.port_host + "portal/iserver/auth/status", verify = False)
         if(resp.status_code != 200):
-            print('error')
+            print('auth error...restarting server')
+            self.start_session()
+        if(resp.json()["authenticated"]):
+            self.auth = True
+            print("authenticaed")
+        else:
+            print("need to re-authenticate")
+            
 
     #Shuts server down
     def __kill_server(self):
