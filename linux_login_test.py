320f0b1e57274041396b630f10f17c88fa560729
Brigham Andersen <brighamband@gmail.com>
2020-07-09 13:57:07 -0600

    Updated linux login file

---
+++ linux_login_test.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+import os
+
+#Settings to get past chrome local host privacy
+options = webdriver.ChromeOptions()
+options.binary_location = "./chromium-latest-linux/run.sh"
+options.add_argument('--ignore-ssl-errors=yes')
+options.add_argument('--ignore-certificate-errors')
+
+#driver = webdriver.Chrome(executable_path=r'./chromium-latest-linux/run.sh', options=options)
+driver = webdriver.Chrome(executable_path=r'./chromedriver_linux', options=options)
+driver.get('google.com')
+# driver.get("https://localhost:5000")
+
+'''
+# username = "samsv1875"
+# password = "Sis4samuel"
+username_textbox = driver.find_element_by_id("user_name")
+username_textbox.send_keys(username)
+password_textbox = driver.find_element_by_id("password")
+password_textbox.send_keys(password)
+
+login_button = driver.find_element_by_id("submitForm")
+login_button.submit()
+'''
