b8143bfdfea364428bf571062ec966f46a8b0476
Brigham Andersen <brighamband@gmail.com>
2020-07-12 12:01:43 -0600

    minor

--- linux_login_test.py
+++ linux_login_test.py
@@ -1,14 +1,15 @@
 from selenium import webdriver
 import os
 
-#Settings to get past chrome local host privacy
+# Settings to get past chrome local host privacy
 options = webdriver.ChromeOptions()
 options.binary_location = "./chromium-latest-linux/run.sh"
 options.add_argument('--ignore-ssl-errors=yes')
 options.add_argument('--ignore-certificate-errors')
 
 #driver = webdriver.Chrome(executable_path=r'./chromium-latest-linux/run.sh', options=options)
-driver = webdriver.Chrome(executable_path=r'./chromedriver_linux', options=options)
+driver = webdriver.Chrome(
+    executable_path=r'./chromedriver_linux', options=options)
 driver.get('google.com')
 # driver.get("https://localhost:5000")
 
--- server.py
+++ server.py
@@ -6,9 +6,10 @@ import requests
 import json
 from selenium import webdriver
 
-#Disable insecure request warnings from verify = False in requests
+# Disable insecure request warnings from verify = False in requests
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 
 class Server:
     def __init__(self):
@@ -21,23 +22,23 @@ class Server:
         self.PAPER_USERNAME = 'byusf3215'
         self.PAPER_PASSWORD = 'paper1234'
 
-    #Starts server
+    # Starts server
     def start_session(self):
 
         strt_cmd = [r'gnome-terminal', '--', r'bin/run.sh', r'root/conf.yaml']
-        self.process = subprocess.Popen(args = strt_cmd, cwd = self.gateway_path)
+        self.process = subprocess.Popen(args=strt_cmd, cwd=self.gateway_path)
         #print("session started")
-        #time.sleep(3)
-        #self.__kill_server()
+        # time.sleep(3)
+        # self.__kill_server()
 
-    #Submits https object and returns response
+    # Submits https object and returns response
     def __submit(self):
         pass
 
-    #Checks server state given it is access
+    # Checks server state given it is access
     def check_status(self):
-        #Confirm server is active
-        resp = requests.post(self.port_host + "portal/tickle", verify = False)
+        # Confirm server is active
+        resp = requests.post(self.port_host + "portal/tickle", verify=False)
         if(resp.status_code != 200):
             print('tickle error')
         if(resp.json()['iserver']['authStatus']['connected']):
@@ -47,13 +48,15 @@ class Server:
             print('not connected')
             self.active = False
 
-        #Check validation
-        resp = requests.get(self.port_host + "portal/sso/validate", verify = False)
+        # Check validation
+        resp = requests.get(
+            self.port_host + "portal/sso/validate", verify=False)
         if(resp.status_code != 200):
             print('validate error')
 
-        #Check authentication status
-        resp = requests.post(self.port_host + "portal/iserver/auth/status", verify = False)
+        # Check authentication status
+        resp = requests.post(
+            self.port_host + "portal/iserver/auth/status", verify=False)
         if(resp.status_code != 200):
             print('auth error...restarting server')
             self.start_session()
@@ -62,9 +65,9 @@ class Server:
             print("authenticaed")
         else:
             print("need to re-authenticate")
-            
 
-    #Shuts server down
+    # Shuts server down
+
     def __kill_server(self):
 
         pid = self.process.pid
@@ -72,24 +75,22 @@ class Server:
             id.kill()
         pid.kill()
 
-        
-    #Automates login process
+    # Automates login process
+
     def __login(self):
         # FIXME - change extension for Linux
-        self.driver = webdriver.Chrome('C:\\Coding\SilverFund\selenium_testing\chromedriver', options=options)
+        self.driver = webdriver.Chrome(
+            'C:\\Coding\SilverFund\selenium_testing\chromedriver', options=options)
         self.driver.get('https://localhost:5000')
-        userName = self.driver.find_element_by_id('user_name').send_keys(self.PAPER_USERNAME)
-        password = self.driver.find_element_by_id('password').send_keys(self.PAPER_PASSWORD)
+        userName = self.driver.find_element_by_id(
+            'user_name').send_keys(self.PAPER_USERNAME)
+        password = self.driver.find_element_by_id(
+            'password').send_keys(self.PAPER_PASSWORD)
         loginButton = self.driver.find_element_by_id('submitForm').click()
         # FIXME - do we want to quit out of the selenium driver at this point?
         # time.sleep(4)
         # self.driver.quit()
 
 
-
-    
-
 server = Server()
 server.check_status()
-
-
