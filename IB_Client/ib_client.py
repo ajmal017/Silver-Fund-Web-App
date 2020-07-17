b8ab4c5f9a852d9cab8d61802884c3cd4df9504a
Samuel Earnest <earnestcsamuel@gmail.com>
2020-07-17 13:04:41 -0600

    sam

--- IB_Client/ib_client.py
+++ IB_Client/ib_client.py
@@ -90,3 +90,89 @@ class IB_Client():
 
         """
         return None
+
+    def get_margin_cash(self, account_num):
+        """Returns current cash margin
+
+        Parameters
+        ----------
+        account_num : int
+            The account id associated with the given interactive brokers account
+
+        Returns
+        -------
+        double
+            cash margin in USD
+
+        """
+        return None
+
+    def get_margin_portfolio(self, account_num):
+        """Returns current portfolio margin
+
+        Parameters
+        ----------
+        account_num : int
+            The account id associated with the given interactive brokers account
+
+        Returns
+        -------
+        double
+            portfolio margin in USD
+
+        """
+        return None
+
+    def get_short_callbacks(self, account_num):
+        """Returns all shorts that are being called back today.
+
+        Parameters
+        ----------
+        account_num : int
+            The account id associated with the given interactive brokers account
+
+        Returns
+        -------
+        [position]
+            all positions being called back today
+
+        """
+        return None
+
+    def get_assets(self, look_up_array):
+        """Looks up info for given assets and returns them in an array of assets.
+
+        Parameters
+        ----------
+        look_up_array : string []
+            An array of tickers, cusips, or conids?
+
+        Returns
+        -------
+        asset[]
+            Returns an array of requested assets.
+
+        """
+        return None
+
+    def get_assets_on(self, day, month, year, look_up_array):
+        """Looks up info for given assets on given date and returns them in an array of assets.
+
+        Parameters
+        ----------
+        year : int
+            Four diget representing the year. example: 2021.
+        month : int
+            Two digets representing the month. example: 03 for the month of March.
+        day : int
+            Two digets representing the day. example: 08 for the 8th.
+        look_up_array : string []
+            An array of tickers, cusips, or conids?
+
+        Returns
+        -------
+        type
+            Description of returned object.
+
+        """
+        return None
