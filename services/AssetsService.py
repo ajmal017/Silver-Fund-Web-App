a2c3aa3106ce8135e8e29a1a3706e27d54b77054
Samuel Earnest <earnestcsamuel@gmail.com>
2020-07-21 13:01:28 -0600

    sam

---
+++ services/AssetsService.py
@@ -0,0 +1,6 @@
+import json
+
+class AssetsService():
+
+    def __init__():
+        return None
---
+++ services/TradesService.py
@@ -0,0 +1,18 @@
+import json
+
+class TradesService():
+
+    def __init__(self):
+        return None
+
+    def get_current(self):
+        return None
+
+    def get_on(self, year, month, day):
+        return None
+
+    def get_unsettled(self):
+        return None
+
+    def update(self):
+        return None
