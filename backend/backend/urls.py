59bdce4f0abe255317db6c58d79b1eae5f39a63a
Brigham Andersen <brighamband@gmail.com>
2020-07-31 14:44:21 -0600

    fixed urls (typo, trailing slashes)

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,19 +20,18 @@ from api import views
 from django.contrib import admin
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-router.register(r'postions', views.PositionViewSet)
+router.register(r"users", views.UserViewSet)
+router.register(r"groups", views.GroupViewSet)
+router.register(r"postions", views.PositionViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('admin/', admin.site.urls),
-    
-    path("api/postitions/current", views.get_cur_positions),
-    path("api/trades/current", views.get_cur_trades),
-    path("api/trades/unsettled", views.get_unsettled_trades),
-    path("api/ibaccount/cashbalance", views.get_cashbalance),
+    path("", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("admin/", admin.site.urls),
+    path("api/positions/current/", views.get_cur_positions),
+    path("api/trades/current/", views.get_cur_trades),
+    path("api/trades/unsettled/", views.get_unsettled_trades),
+    path("api/ibaccount/cashbalance/", views.get_cashbalance),
 ]
