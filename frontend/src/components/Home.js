5689fa8091837a66a67f71ed385d6086d7179a69
Brigham Andersen <brighamband@gmail.com>
2020-07-31 10:40:32 -0600

    home page and layout working

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import "../style.css";
+import tradingImg from "../images/transparent-trading.png";
+
+function Home() {
+  return (
+    <>
+      <main role="main" className="inner cover">
+        <img src={tradingImg} alt="" />
+        <h1 className="cover-heading">Silver Fund Web App</h1>
+        <p className="lead">
+          An institutional-quality web app created for use by Silver Fund, The
+          Marriott Business School's student-run investment fund. This app aims
+          to support investment research, trading, risk management, and
+          portfolio analysis.
+        </p>
+        <p className="lead-button">
+          <a
+            href="https://marriottschool.byu.edu/mba/about/accelerated-learning/silver-fund/"
+            className="btn btn-lg btn-secondary"
+          >
+            Learn More
+          </a>
+        </p>
+      </main>
+    </>
+  );
+}
+
+export default Home;
---
+++ frontend/src/components/Layout.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "../style.css";
+
+class Layout extends React.Component {
+  render() {
+    return (
+      <>
+        {/* HEADER */}
+        <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+          <header className="masthead mb-auto">
+            <div className="inner">
+              <h3 className="masthead-brand">BYU Silver Fund</h3>
+              <nav className="nav nav-masthead justify-content-center">
+                <a className="nav-link active" href="#">
+                  Home
+                </a>
+                <a className="nav-link" href="#">
+                  Features
+                </a>
+                <a className="nav-link" href="#">
+                  Contact
+                </a>
+              </nav>
+            </div>
+          </header>
+        </div>
+
+        {/* CONTENT */}
+        <div className="content">{this.props.children}</div>
+
+        {/* FOOTER */}
+        <footer>
+          <div className="inner">
+            <p>&copy; Silver Fund | All Rights Reserved</p>
+          </div>
+        </footer>
+      </>
+    );
+  }
+}
+
+export default Layout;
