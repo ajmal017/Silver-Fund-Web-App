37ac1a8b67cb3bae111f14b1c45c68d7c17ca34d
Brigham Andersen <brighamband@gmail.com>
2020-08-03 11:12:41 -0600

    lower case routes

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Route } from "react-router-dom";
 
 import HomeSignin from "./components/HomeSignin";
+import Signup from "./components/Signup";
 
 const BaseRouter = () => (
   <div>
     <Route exact path="/" component={HomeSignin} />
+    <Route exact path="/" component={Signup} />
   </div>
 );
 
---
+++ frontend/src/components/Signup.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+import tradingImg from "../images/trading-trans-white.png";
+
+function Signup() {
+  return (
+    <div className="card" id="signup-box">
+      <h3 className="card-title">Welcome Back!</h3>
+      <form>
+        {/* Username */}
+        <div className="input-group mb-3">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="basic-addon1">
+              @
+            </span>
+          </div>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+          ></input>
+        </div>
+        {/* Password */}
+        <div className="form-group">
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+          />
+        </div>
+        {/* Sign In */}
+        <button type="submit" className="btn btn-dark" id="signin-btn">
+          Sign In
+        </button>
+        <p className="forgot-password text-right">
+          <a href="#">Forgot password?</a>
+        </p>
+        <h5>Need an account?</h5>
+        <a class="btn btn-dark" href="#" role="button" id="signup-btn">
+          Sign Up
+        </a>
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
