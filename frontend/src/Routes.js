2b69d6ec12e440a9f7170351f6445be279359cad
Brigham Andersen <brighamband@gmail.com>
2020-08-06 17:32:27 -0600

    Got trades working

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,16 +3,18 @@ import { Route } from "react-router-dom";
 
 import HomeSignin from "./pages/HomeSignin";
 import Signup from "./pages/Signup";
-import Positions from "./pages/Positions";
 import CPTable from "./components/CPTable";
+import Positions from "./pages/Positions";
+import Trades from "./pages/Trades";
 
 function Routes() {
   return (
     <div className="content">
       <Route exact path="/" component={HomeSignin} />
       <Route exact path="/signup/" component={Signup} />
-      <Route exact path="/dbpositions/" component={Positions} />
       <Route exact path="/ibpositions/" component={CPTable} />
+      <Route exact path="/dbpositions/" component={Positions} />
+      <Route exact path="/dbtrades/" component={Trades} />
     </div>
   );
 }
---
+++ frontend/src/pages/Trades.js
@@ -0,0 +1,75 @@
+import React from "react";
+import axios from "axios";
+
+class Trades extends React.Component {
+  state = {
+    data: [],
+  };
+
+  fetchTradesData = () => {
+    axios
+      .get("http://localhost:8000/trades/", {
+        auth: {
+          username: "su",
+          password: "su",
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          data: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("There was an error when retrieving the data.", error);
+      });
+  };
+
+  componentDidMount() {
+    this.fetchTradesData();
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Trades</h3>
+        <br />
+        {this.state.data.length > 0 ? (
+          <table>
+            <thead>
+              <tr>
+                <th>Trade ID</th>
+                <th>Asset ID</th>
+                <th>Trade Type</th>
+                <th>Number of Shares</th>
+                <th>Price</th>
+                <th>Total Price</th>
+                <th>Trade Time</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.data.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td key={item.trade_id}>{item.trade_id}</td>
+                    <td key={item.asset_id}>{item.asset_id}</td>
+                    <td key={item.trade_type}>{item.trade_type}</td>
+                    <td key={item.num_of_shares}>{item.num_of_shares}</td>
+                    <td key={item.price}>{item.price}</td>
+                    <td key={item.tot_price}>{item.tot_price}</td>
+                    <td key={item.trade_time}>{item.trade_time}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        ) : (
+          <h4 style={{ color: "red" }}>Loading...</h4>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Trades;
