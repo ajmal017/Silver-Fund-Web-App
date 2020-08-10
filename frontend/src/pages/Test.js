7ada8915219effa91b91b94f4d4f510d9b21a197
Brigham Andersen <brighamband@gmail.com>
2020-08-10 14:35:25 -0600

    got hide/show tables working

--- frontend/src/pages/Test.js
+++ frontend/src/pages/Test.js
@@ -1,30 +1,54 @@
 import React from "react";
 import TableDB from "../components/TableDB";
 
-function Test() {
-  function alertMe() {
-    // <TableDB url={"http://localhost:8000/all_positions/"} />;
-    alert("Test");
+class Test extends React.Component {
+  state = {
+    showCurrent: false,
+    currentCount: 0,
+    showAll: false,
+    allCount: 0,
+  };
+
+  incrementCount() {
+    this.setState({ count: this.state.count + 1 });
   }
 
-  function getTable() {
-    const url = "http://localhost:8000/all_positions/";
+  onClickCurrent = () => {
+    this.setState({ currentCount: this.state.currentCount + 1 });
 
-    console.log(url);
-    // <TableDB url={url} />;
-    return <h1>GET TABLE</h1>;
+    if (this.state.currentCount % 2) {
+      this.setState({ showCurrent: false });
+    } else {
+      this.setState({ showCurrent: true });
+    }
+    console.log(this.state.currentCount);
+  };
+
+  onClickAll = () => {
+    this.setState({ allCount: this.state.allCount + 1 });
+
+    if (this.state.allCount % 2) {
+      this.setState({ showAll: false });
+    } else {
+      this.setState({ showAll: true });
+    }
+    console.log(this.state.allCount);
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.onClickCurrent}>Show Current Positions</button>
+        {this.state.showCurrent ? (
+          <TableDB url="http://localhost:8000/current_positions/" />
+        ) : null}
+        <button onClick={this.onClickAll}>Show All Positions</button>
+        {this.state.showAll ? (
+          <TableDB url="http://localhost:8000/all_positions/" />
+        ) : null}
+      </div>
+    );
   }
-
-  return (
-    <button
-      onClick={() => {
-        console.log("test");
-        return <TableDB url={"http://localhost:8000/all_positions/"} />;
-      }}
-    >
-      Test
-    </button>
-  );
 }
 
 export default Test;
