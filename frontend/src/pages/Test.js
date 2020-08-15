8b326b4a60e8120fb8c8a7ee43a10a8c2948f4fd
Brigham Andersen <brighamband@gmail.com>
2020-08-14 19:57:32 -0600

    still testing react-select

--- frontend/src/pages/Test.js
+++ frontend/src/pages/Test.js
@@ -1,25 +1,39 @@
 import React, { useState } from "react";
-import DateRanger from "../components/DateRanger";
+import Select from "react-select";
 
-function Test() {
-  const [start, setStart] = useState("");
-  const [end, setEnd] = useState("");
-  const [ready, setReady] = useState(false);
+const tickers = [
+  { value: "AAPL", label: "AAPL  (Apple)" },
+  { value: "FB", label: "FB  (Facebook)" },
+  { value: "33L", label: "33L  (Lulu)" },
+  { value: "TSLA", label: "TSLA  (Tesla)" },
+];
+
+function Test(props) {
+  const [tickersFilter, setTickersFilter] = useState([0, 2, 3]);
+
+  var test = 1;
 
   return (
-    <div>
-      <DateRanger
-        onStartChange={(value) => setStart(value)}
-        onEndChange={(value) => setEnd(value)}
-        onSubmit={(value) => setReady(value)}
+    <>
+      <Select
+        isMulti
+        // name="colors"
+        options={tickers}
+        className="basic-multi-select"
+        classNamePrefix="select"
+        // onChange={() => setTickersFilter([0, 3, 6])}
+        // onChange=
+        // onChange={(event) => console.log("event ", event.label)}
       />
-      <hr />
-      {ready && (
-        <div>
-          {start} {end}}
-        </div>
-      )}
-    </div>
+      <span onClick={() => setTickersFilter([test, 3, 6])}>CLICKME</span>
+      {tickersFilter}
+      Numbers:
+      <ul>
+        {tickersFilter.map((ticker, i) => {
+          return <li key={i}>{ticker}</li>;
+        })}
+      </ul>
+    </>
   );
 }
 
