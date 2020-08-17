ad6765ca497125f5276d53e7c69882af5a4b8426
Brigham Andersen <brighamband@gmail.com>
2020-08-17 11:34:47 -0600

    Testing tabs

--- frontend/src/pages/Test.js
+++ frontend/src/pages/Test.js
@@ -1,41 +1,64 @@
 import React, { useState } from "react";
-import Select from "react-select";
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
+import Positions from "./Positions";
+import TransactionHistory from "./TransactionHistory";
 
-const tickers = [
-  { value: "AAPL", label: "AAPL  (Apple)" },
-  { value: "FB", label: "FB  (Facebook)" },
-  { value: "33L", label: "33L  (Lulu)" },
-  { value: "TSLA", label: "TSLA  (Tesla)" },
-];
-
-function Test(props) {
-  const [tickersFilter, setTickersFilter] = useState([0, 2, 3]);
-
-  var test = 1;
-
+function Test() {
   return (
     <>
-      <Select
-        isMulti
-        // name="colors"
-        options={tickers}
-        className="basic-multi-select"
-        classNamePrefix="select"
-        // onChange={() => setTickersFilter([0, 3, 6])}
-        // onChange=
-        // onChange={(event) => console.log("event ", event.label)}
-      />
-      <span onClick={() => setTickersFilter([test, 3, 6])}>CLICKME</span>
-      {tickersFilter}
-      Numbers:
-      <ul>
-        {tickersFilter.map((ticker, i) => {
-          return <li key={i}>{ticker}</li>;
-        })}
-      </ul>
+      <Tabs defaultActiveKey="home">
+        <Tab eventKey="home" title="Home">
+          <h1>Welcome Home!</h1>
+        </Tab>
+        <Tab eventKey="profile" title="Positions">
+          <Positions />
+        </Tab>
+        <Tab eventKey="contact" title="Transaction History">
+          <TransactionHistory />
+        </Tab>
+      </Tabs>
     </>
   );
 }
+
+// import Select from "react-select";
+
+// const tickers = [
+//   { value: "AAPL", label: "AAPL  (Apple)" },
+//   { value: "FB", label: "FB  (Facebook)" },
+//   { value: "33L", label: "33L  (Lulu)" },
+//   { value: "TSLA", label: "TSLA  (Tesla)" },
+// ];
+
+// function Test(props) {
+//   const [tickersFilter, setTickersFilter] = useState([0, 2, 3]);
+
+//   var test = 1;
+
+//   return (
+//     <>
+//       <Select
+//         isMulti
+//         // name="colors"
+//         options={tickers}
+//         className="basic-multi-select"
+//         classNamePrefix="select"
+//         // onChange={() => setTickersFilter([0, 3, 6])}
+//         // onChange=
+//         // onChange={(event) => console.log("event ", event.label)}
+//       />
+//       <span onClick={() => setTickersFilter([test, 3, 6])}>CLICKME</span>
+//       {tickersFilter}
+//       Numbers:
+//       <ul>
+//         {tickersFilter.map((ticker, i) => {
+//           return <li key={i}>{ticker}</li>;
+//         })}
+//       </ul>
+//     </>
+//   );
+// }
 
 // import DateRangePicker from "@wojtekmaj/react-daterange-picker";
 
