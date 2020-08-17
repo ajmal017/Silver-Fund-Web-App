9b745e98e99433ba7139a565afb76c3fa0b066a6
Brigham Andersen <brighamband@gmail.com>
2020-08-17 11:17:59 -0600

    Don't have Trades anymore (TH instead)

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -4,7 +4,6 @@ import { Route } from "react-router-dom";
 import HomeSignin from "./pages/HomeSignin";
 import Signup from "./pages/Signup";
 import Positions from "./pages/Positions";
-import Trades from "./pages/Trades";
 import TransactionHistory from "./pages/TransactionHistory";
 import Test from "./pages/Test";
 
@@ -14,7 +13,6 @@ function Routes() {
       <Route exact path="/" component={HomeSignin} />
       <Route exact path="/signup/" component={Signup} />
       <Route exact path="/positions/" component={Positions} />
-      <Route exact path="/trades/" component={Trades} />
       <Route exact path="/transactionhistory/" component={TransactionHistory} />
       <Route exact path="/test/" component={Test} />
     </div>
