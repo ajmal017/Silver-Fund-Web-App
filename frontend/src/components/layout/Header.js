07ff8503e31587f3ee9795795c57a988f13ef513
Brigham Andersen <brighamband@gmail.com>
2020-08-21 12:07:45 -0600

    Added BYU to nav, sf logo to home

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,17 +1,27 @@
 import React from "react";
 
-import sfLogo from "../../images/sf-logo-notext.png";
+import sfLogo from "../../images/sf-logo.png";
+import byuLogoText from "../../images/byu-logo-text.png";
 
 export default function Header() {
   return (
     <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
       <header className="masthead mb-auto">
         <h3 className="masthead-brand">
-          BYU Silver Fund
+          <img
+            src={byuLogoText}
+            alt=""
+            style={{
+              height: "29px",
+              paddingBottom: "6px",
+              paddingRight: "8px",
+            }}
+          />
+          Silver Fund
           <img
             src={sfLogo}
             alt=""
-            style={{ height: "30px", paddingLeft: "20px", marginTop: "-7px" }}
+            style={{ height: "25px", paddingLeft: "20px", marginTop: "-7px" }}
           />
         </h3>
         <nav className="nav nav-masthead justify-content-center">
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,22 +1,33 @@
 import React from "react";
 
+import sfLogo from "../images/sf-logo.png";
+
 export default function Home() {
   return (
     <div className="content">
-      {/* FIXME - Add their username to this line, verifies to them that they're signed in*/}
-      <h3>Welcome to the Silver Fund Web App!</h3>
-      <p className="intro-info">
-        Click the tabs above to navigate between panes. <br />
-        See the buttons with links below to learn more about how to use the app.
-      </p>
-      <a
-        className="btn"
-        href="https://byu.sharepoint.com/sites/silverfund-wiki"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Documentation
-      </a>
+      <div className="left-col">
+        {/* FIXME - Add their username to this line, verifies to them that they're signed in*/}
+        <h3>Welcome to the Silver Fund Web App!</h3>
+        <p className="intro-info">
+          Click the tabs above to navigate between panes. <br />
+          See the buttons with links below to learn more about how to use the
+          app.
+        </p>
+        <a
+          className="btn"
+          href="https://byu.sharepoint.com/sites/silverfund-wiki"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Documentation
+        </a>
+      </div>
+      <img
+        src={sfLogo}
+        className="home-logo"
+        alt=""
+        style={{ width: "600px" }}
+      />
     </div>
   );
 }
