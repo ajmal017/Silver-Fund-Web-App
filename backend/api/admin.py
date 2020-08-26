dd31722be96b4f61223619cc1d0471c7b03a6d58
Brigham Andersen <brighamband@gmail.com>
2020-08-25 18:09:14 -0600

    custom admin names

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 from api.models import Position
 
-# Register your models here.
+admin.site.site_header = "Admin - Silver Fund"
+admin.site.site_title = "Silver Fund"
 
 admin.site.register(Position)
 admin.site.unregister(Group)
