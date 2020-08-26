351336a22cfc73d9ef203baa38ad4e3c49390b0d
Brigham Andersen <brighamband@gmail.com>
2020-08-25 20:15:01 -0600

    Improved admin (list view, add, and edit)

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,10 +1,51 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from api.models import Position
 
+
+admin.site.unregister(Group)
+admin.site.unregister(User)
 admin.site.site_header = "Admin - Silver Fund"
 admin.site.site_title = "Silver Fund"
 
+
+class UserAdmin(BaseUserAdmin):
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "is_staff",
+    )
+    fieldsets = ()
+    fields = (
+        "first_name",
+        "last_name",
+        "username",
+        "email",
+        "password",
+        "is_staff",
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "username",
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                ),
+            },
+        ),
+    )
+
+
 admin.site.register(Position)
-admin.site.unregister(Group)
+admin.site.register(User, UserAdmin)
 
