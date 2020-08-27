1b40d0e469c290adbc53ceeaabcecdd3ca33c987
Brigham Andersen <brighamband@gmail.com>
2020-08-27 14:12:05 -0600

    Improved date ranger and singler

--- frontend/src/pages/components/DateRanger.js
+++ frontend/src/pages/components/DateRanger.js
@@ -4,27 +4,25 @@ import { getDateStr } from "../../helpers";
 
 export default function DateRanger(props) {
   return (
-    <>
-      <label className="date-label">
-        Start Date:
-        <input
-          type="date"
-          className="date-input start-date"
-          value={props.start}
-          max={getDateStr(0)}
-          onChange={(event) => props.onStartChange(event.target.value)}
-        />
-      </label>
-      <label className="date-label">
-        End Date:
-        <input
-          type="date"
-          className="date-input end-date"
-          value={props.end}
-          max={getDateStr(0)}
-          onChange={(event) => props.onEndChange(event.target.value)}
-        />
-      </label>
-    </>
+    <div style={{ width: "250px", height: "79px" }} className="m-2">
+      <label className="input-label">Start Date: </label>
+      <input
+        type="date"
+        className="date-input"
+        value={props.start}
+        max={getDateStr(0)}
+        onChange={(event) => props.onStartChange(event.target.value)}
+      />
+      <br />
+      <br />
+      <label className="input-label">End Date:</label>
+      <input
+        type="date"
+        className="date-input"
+        value={props.end}
+        max={getDateStr(0)}
+        onChange={(event) => props.onEndChange(event.target.value)}
+      />
+    </div>
   );
 }
--- frontend/src/pages/components/DateSingler.js
+++ frontend/src/pages/components/DateSingler.js
@@ -4,19 +4,17 @@ import { getDateStr } from "../../helpers";
 
 export default function DateSingler(props) {
   return (
-    <>
-      <label className="date-label" style={{ width: "200px" }}>
-        Date:
-        <input
-          type="date"
-          className="date-input one-date"
-          value={props.date}
-          max={getDateStr(0)}
-          onChange={(event) => {
-            props.onDateChange(event.target.value);
-          }}
-        />
-      </label>
-    </>
+    <div style={{ width: "200px", height: "30px" }} className="m-2 mb-4">
+      <label className="input-label">Date:</label>
+      <input
+        type="date"
+        className="date-input one-date"
+        value={props.date}
+        max={getDateStr(0)}
+        onChange={(event) => {
+          props.onDateChange(event.target.value);
+        }}
+      />
+    </div>
   );
 }
