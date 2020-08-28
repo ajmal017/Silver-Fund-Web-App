9d04a4eb113b4fc7f143d632a74ae6b5edcedb5b
Brigham Andersen <brighamband@gmail.com>
2020-08-28 15:38:04 -0600

    improved script

--- backend/create_users/script.py
+++ backend/create_users/script.py
@@ -1,14 +1,10 @@
 import csv, sys, os, django
 
-
 project_dir = "C://Coding/SF/Silver-Fund-Web-App/backend"
 sys.path.append(project_dir)
 os.environ["DJANGO_SETTINGS_MODULE"] = "backend.settings"
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", __file__)
-import django
-
 django.setup()
-
 
 from django.contrib.auth import authenticate
 from django.contrib import admin
@@ -24,13 +20,24 @@ User = get_user_model()
 file = "users-fall-2020.csv"
 
 data = csv.reader(open(file), delimiter=",")
+
+# print("")
+print("\nResults:\n")
+# print("")
+index = 0
+
 for row in data:
-    Post = User()
-    Post.first_name = row[0]
-    Post.last_name = row[1]
-    Post.username = row[2]
-    Post.email = row[3]
-    Post.password = make_password(row[4])
-    Post.is_active = "0"
-    Post.save()
-    print("success")
+    index = index + 1
+
+    new_user = User()
+    new_user.first_name = row[0]
+    new_user.last_name = row[1]
+    new_user.username = row[2]
+    new_user.email = row[3]
+    new_user.password = make_password(row[4])
+    new_user.is_active = "0"
+    new_user.save()
+
+    print("Successfully created user (" + str(index) + ").")
+
+print("\n" + str(index) + " users created.")
